d9e105e25a28050e4195d0e8e5d2e17ec3db7e9b
shavonthadani <shavon.thadani@gmail.com>
2021-05-20 23:02:19 -0400

    Added volcano markers
    Used pandas to read Volcanoes.txt as a csv file. Then got lon and lat from the data frame. Used them in the loop to place the volcano markers

--- map1.py
+++ map1.py
@@ -1,13 +1,18 @@
 #imports
 import folium
+import pandas
 
+#getting and formatting data
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
 #initialize objects
 map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-#Add test markers to feature group and then to map
-for coordinates in [[38.2,-99.1], [39.2,-97.1]]:
-    fg.add_child(folium.Marker(location=coordinates,popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+#Add volcano markers to feature group and then to map
+for lt, ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 #create html map file
