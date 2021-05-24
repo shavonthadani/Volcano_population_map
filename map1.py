7e2892cee3e2e57a0f95f0bfb2f828e245757aae
shavonthadani <shavon.thadani@gmail.com>
2021-05-24 19:12:01 -0400

    Description
    Added the name and elevation to the markers. Also changed the colour of the marker depending on its elevation.

--- map1.py
+++ map1.py
@@ -2,17 +2,29 @@
 import folium
 import pandas
 
+
 #getting and formatting data
 data = pandas.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def elevColour(elev):
+    if el < 1000:
+        return "green"
+    elif el > 1000 and el < 2000:
+        return "orange"
+    else:
+        return "red"
 #initialize objects
-map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles="Stamen Terrain")
+map = folium.Map(location=[38.58,-99.09],zoom_start=6,tiles='Stamen Terrain')
 fg = folium.FeatureGroup(name="My Map")
 
 #Add volcano markers to feature group and then to map
-for lt, ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el, nm in zip(lat,lon, elev,name):
+    fg.add_child(folium.Marker(location=[lt,ln],popup= nm + "\n elevation:" + str(el) + " m", icon=folium.Icon(color=elevColour(el))))
+
 map.add_child(fg)
 
 #create html map file
